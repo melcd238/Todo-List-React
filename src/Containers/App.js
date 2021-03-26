fb305444f0c15df57a4346aed52bed363ae12564
melcd238 <melcd238@gmail.com>
2021-03-26 15:40:52 +0100

    mise en place Axios et Firebase

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './App.css';
 import TodoTask from '../Components/TodoTask'
+import axios from '../Axios/Axios.firebase'
 
 function App() {
-  // State 
+  // States:
   const [task, setTask]=useState([])
 
   const [input ,setInput]=useState(" ")
+
+  // Ref:
+
+  const inputRef = useRef()
+  // ETATS: 
+  useEffect(()=>{
+    console.log('[App.js] useEffect');
+    inputRef.current.focus();
+    fetchTask();
+   
+    
+    return()=>{
+      console.log('[App.js] useEffect (didUnmount)');
+    }
+  
+  },[])
 
   // Variables:
 
@@ -26,6 +43,13 @@ function App() {
       const newtask = [...task];
       newtask[index].done = !task[index].done;
       setTask(newtask)
+      axios.put('task/' + task[index].id + '.json', newtask[index])
+      .then(response=>{
+        console.log(response)
+      })
+      .catch(error=>{
+        console.log(error)
+      })
   }
 
   
@@ -33,6 +57,13 @@ function App() {
     const newTask = [...task];
    newTask.splice(index,1)
     setTask(newTask)
+    axios.delete('task/' + task[index].id + '.json')
+    .then(response=>{
+      console.log(response)
+    })
+    .catch(error=>{
+      console.log(error)
+    })
   }
   const submitHandler = (event)=>{
     event.preventDefault()
@@ -41,11 +72,36 @@ function App() {
       content : input,
       done : false
     }
-    setTask([...task,newTask])
     setInput('')
+    axios.post('task.json', newTask)
+    .then(response=>{
+       console.log(response)
+       fetchTask();
+    })
+    .catch(error=>{
+      console.log(error)
+    })
   }
   const changedInputHandler = (event)=>{
      setInput(event.target.value);
+  }
+
+  const fetchTask = ()=>{
+    axios.get('task.json')
+    .then(response=>{
+      const newTasks=[]
+      for(let key in response.data){
+          newTasks.push({
+            ...response.data[key],
+            id : key
+          })
+      }
+      setTask(newTasks)
+
+    })
+    .catch(error=>{
+      console.log(error)
+    })
   }
 
   return (
@@ -54,6 +110,7 @@ function App() {
        <div className="todo">
          <form onSubmit={(e)=> submitHandler(e)}>
          <input className="inputTodo"
+                ref={inputRef}
                 value={input}
                 placeholder="Que souhaitez-vous ajouter?" 
                 type="text"
